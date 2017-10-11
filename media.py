5056becef3c954bd631eceea38d4a4dbb1dd81de
Omar Gaber <omargaber956@gmail.com>
2017-10-11 06:42:14 +0200

    Update media.py

--- media.py
+++ media.py
@@ -1,8 +1,7 @@
 class Movie():
-
-	#Movie object constructor
-	def __init__(self, movie_title, movie_storyline, movie_poster, movie_trailer):
-		self.title = movie_title
-		self.storyline = movie_storyline
-		self.poster_image_url = movie_poster
-		self.trailer_youtube_url = movie_trailer
+    # Movie object constructor
+    def __init__(self, movie_title, movie_storyline, movie_poster, movie_trailer):
+        self.title = movie_title
+        self.storyline = movie_storyline
+        self.poster_image_url = movie_poster
+        self.trailer_youtube_url = movie_trailer
